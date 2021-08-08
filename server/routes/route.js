766b70bdbe31f2c130e088c6d7d3cc0ee19edb3a
SKS <shaha460@gmail.com>
2021-08-09 00:41:42 +0600

    get function from controller to routes

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import { createPost, getPost } from '../controller/controller.js'
 
 const router = express.Router()
 
-router.get('/', )
+router.get('/', getPost)
+router.post('/', createPost)
 
 export default router
