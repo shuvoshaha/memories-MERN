91b6bb2d9ac00cbe6a18c37eadd2b7d6f2a9b40c
SKS <shaha460@gmail.com>
2021-08-09 00:40:00 +0600

    create post into db

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -3,10 +3,23 @@ import postMessage from "../model/postMessage.js";
 // get post from db
 export const getPost =  async (req, res) =>{
     try{
-        const data = postMessage.find()
+        const data = await postMessage.find()
         res.status(200).json(data)
     }
 
+    catch(err){
+        res.status(404).json({ message: err.message })
+    }
+}
+
+// create post into db
+export const createPost = async (req, res) =>{
+    const formData = req.body;
+    const newData = new postMessage(formData)
+
+    try{
+        res.status(200).json(newData)
+    }
     catch(err){
         res.status(404).json({ message: err.message })
     }
