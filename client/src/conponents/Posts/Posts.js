02eda7dac9cfec530a1a12ee52def0c584cffd93
SKS <shaha460@gmail.com>
2021-08-09 10:30:16 +0600

    get data from state using useSelector

--- client/src/conponents/Posts/Posts.js
+++ client/src/conponents/Posts/Posts.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import Post from './post/Post'
+import { useSelector } from 'react-redux'
 
 const Posts = () => {
+
+    const state = useSelector(state => state.posts)
+    console.log(state)
+
     return (
         <div className="posts">
             <h2>Posts</h2>
